c1a3999b33e5322b5716addfb03502415f402880
Vu Duc Tuyen <tuyen@naxam.net>
2018-09-17 13:09:16 +0700

    Add small sample

--- qs/LocationLayerQs/Properties/AssemblyInfo.cs
+++ qs/LocationLayerQs/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using Android.App;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Xbindings.MapboxLocationLayer.Droid")]
-[assembly: AssemblyDescription("Xamarin.Android binding library for Mapbox LocationLayer plugin")]
+[assembly: AssemblyTitle("LocationLayerQs")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NAXAM CO.,LTD")]
 [assembly: AssemblyProduct("")]
@@ -18,7 +18,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.8.0.1")]
+[assembly: AssemblyVersion("1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
